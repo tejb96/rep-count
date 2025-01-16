4953868814afca28cdaef97b4d26263194d7ff14
tejb96 <tejpreetbal@gmail.com>
2025-01-16 15:21:52 -0700

    added google strategy,and jwt

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
 import connectDB from "./mongodb/connect.js";
+import passport from "passport";
+
 
 dotenv.config();
 
@@ -13,11 +15,23 @@ app.get('/', (req, res) => {
     res.send({message: 'app running'});
 })
 
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/' }),
+    (req, res) => {
+        // Successful authentication, redirect home.
+        res.redirect('/');
+    }
+);
+
 const port = process.env.PORT || 8080;
 
 const startserver = async() => {
     try{
-        connectDB(process.env.MONGODB_URI);
+        connectDB(process.env.MONGODB_URL);
         app.listen(port, () => {
             console.log('Listening on port ' + port);
         })

5f4a16833bad3897f594625325ed2ae62550f2a1
tejb96 <tejpreetbal@gmail.com>
2025-01-16 15:37:19 -0700

    rm local and fb strategy fields from user model

--- backend/mongodb/models/User.js
+++ backend/mongodb/models/User.js
@@ -1,46 +1,13 @@
-import fs from 'fs';
-import { join } from 'path';
 import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import Joi from 'joi';
-import { isValidUrl } from '../../utils/utils.js';
 // import { IMAGES_FOLDER_PATH } from '../utils/constants';
 
 const { Schema } = mongoose;
 
 const userSchema = new Schema(
     {
-        provider: {
-            type: String,
-            required: true,
-        },
-        username: {
-            type: String,
-            lowercase: true,
-            unique: true,
-            required: [true, "can't be blank"],
-            match: [/^[a-zA-Z0-9_]+$/, 'is invalid'],
-            index: true,
-        },
-        email: {
-            type: String,
-            lowercase: true,
-            unique: true,
-            required: [true, "can't be blank"],
-            match: [/\S+@\S+\.\S+/, 'is invalid'],
-            index: true,
-        },
-        password: {
-            type: String,
-            trim: true,
-            minlength: 6,
-            maxlength: 60,
-        },
         name: String,
-        avatar: String,
         role: { type: String, default: 'USER' },
-        bio: String,
         // google
         googleId: {
             type: String,
@@ -54,20 +21,8 @@ const userSchema = new Schema(
 
 
 userSchema.methods.toJSON = function () {
-    // if not exists avatar1 default
-    const absoluteAvatarFilePath = `${join(__dirname, '../..', IMAGES_FOLDER_PATH)}${this.avatar}`;
-    const avatar = isValidUrl(this.avatar)
-        ? this.avatar
-        : fs.existsSync(absoluteAvatarFilePath)
-            ? `${IMAGES_FOLDER_PATH}${this.avatar}`
-            : `${IMAGES_FOLDER_PATH}avatar2.jpg`;
-
     return {
         id: this._id,
-        provider: this.provider,
-        email: this.email,
-        username: this.username,
-        avatar: avatar,
         name: this.name,
         role: this.role,
         createdAt: this.createdAt,
@@ -79,16 +34,13 @@ const isProduction = process.env.NODE_ENV === 'production';
 const secretOrKey = isProduction ? process.env.JWT_SECRET_PROD : process.env.JWT_SECRET_DEV;
 
 userSchema.methods.generateJWT = function () {
-    const token = jwt.sign(
+    return jwt.sign(
         {
             expiresIn: '6h',
             id: this._id,
-            provider: this.provider,
-            email: this.email,
         },
         secretOrKey,
     );
-    return token;
 };
 
 const User = mongoose.model('User', userSchema);

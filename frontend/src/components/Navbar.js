84853f99d7b5109d4355909c30d216c1f5c13fec
tejb96 <tejpreetbal@gmail.com>
2025-01-17 16:41:55 -0700

    added dark and light theme

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,61 +1,101 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
+import {AppBar, Toolbar, Typography, Button, IconButton, createTheme, ThemeProvider, Box} from '@mui/material';
 import { logOutUser } from '../store/authSlice';
+import CssBaseline from "@mui/material/CssBaseline";
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
 
-const Navbar = ({ children }) => { // Accept children as a prop
+const Navbar = ({ children }) => {
     const auth = useSelector((state) => state.auth);
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'dark');
+
+    useEffect(() => {
+        localStorage.setItem('theme', theme);
+    }, [theme]);
+
+    const darkTheme = createTheme({
+        palette: {
+            mode: 'dark',
+        },
+    });
+
+    const lightTheme = createTheme({
+        palette: {
+            mode: 'light',
+        },
+    });
+
+    const handleToggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+    };
+
+
     const onLogOut = (event) => {
         event.preventDefault();
-        dispatch(logOutUser({ navigate })); // Pass the navigate function as an argument
+        dispatch(logOutUser({ navigate }));
     };
 
     return (
-        <div>
-            <AppBar position="static" sx={{ backgroundColor: 'darkblue', paddingLeft: '30px', paddingRight: '10px', height: '60px' }}>
-                <Toolbar sx={{ display: 'flex', alignItems: 'center' }}>
-                    <Typography variant="h6" sx={{ color: 'white', margin: 0, whiteSpace: 'nowrap' }}>
-                        Workout Tracker
-                    </Typography>
-                    <div style={{ flex: 1 }} />
-                    <div style={{ display: 'flex', alignItems: 'center' }}>
-                        <Button color="inherit" component={Link} to="/">Home</Button>
-                        {auth.isAuthenticated ? (
-                            <>
-                                <Button color="inherit" component={Link} to="/users">Users</Button>
-                                <Button color="inherit" component={Link} to={`/${auth.me.username}`}>Profile</Button>
-                                {auth.me?.role === 'ADMIN' && (
-                                    <Button color="inherit" component={Link} to="/admin">Admin</Button>
-                                )}
-                                <img
-                                    src={auth.me.avatar}
-                                    alt="User Avatar"
-                                    style={{
-                                        width: '30px',
-                                        height: '30px',
-                                        borderRadius: '50%',
-                                        marginLeft: '20px',
-                                        objectFit: 'cover',
-                                    }}
-                                />
-                                <IconButton color="inherit" onClick={onLogOut}>
-                                    Log out
-                                </IconButton>
-                            </>
-                        ) : (
-                            <Button color="inherit" component={Link} to="/login">Login</Button>
-                        )}
-                    </div>
-                </Toolbar>
-            </AppBar>
-            <div>
-                {children} {/* Render the children here */}
-            </div>
-        </div>
+        <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+            <CssBaseline />
+            <Box>
+                <AppBar
+                    position="static"
+                    sx={{
+                        backgroundColor: theme === 'dark' ? 'darkblue' : 'lightblue',
+                        paddingLeft: '30px',
+                        paddingRight: '10px',
+                        height: '60px'
+                    }}
+                >
+                    <Toolbar sx={{ display: 'flex', alignItems: 'center' }}>
+                        <Typography variant="h6" sx={{ color: 'white', margin: 0, whiteSpace: 'nowrap' }}>
+                            Workout Tracker
+                        </Typography>
+                        <div style={{ flex: 1 }} />
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                            <Button color="inherit" component={Link} to="/">Home</Button>
+                            {auth.isAuthenticated ? (
+                                <>
+                                    <Button color="inherit" component={Link} to="/users">Users</Button>
+                                    <Button color="inherit" component={Link} to={`/${auth.me.username}`}>Profile</Button>
+                                    {auth.me?.role === 'ADMIN' && (
+                                        <Button color="inherit" component={Link} to="/admin">Admin</Button>
+                                    )}
+                                    <img
+                                        src={auth.me.avatar}
+                                        alt="User Avatar"
+                                        style={{
+                                            width: '30px',
+                                            height: '30px',
+                                            borderRadius: '50%',
+                                            marginLeft: '20px',
+                                            objectFit: 'cover',
+                                        }}
+                                    />
+                                    <IconButton color="inherit" onClick={onLogOut}>
+                                        Log out
+                                    </IconButton>
+                                </>
+                            ) : (
+                                <Button color="inherit" component={Link} to="/login">Login</Button>
+                            )}
+                            <IconButton color="inherit" onClick={handleToggleTheme}>
+                                {theme === 'light' ? <Brightness4Icon /> : <Brightness7Icon />}
+                            </IconButton>
+                        </div>
+                    </Toolbar>
+                </AppBar>
+                <Box mt={2}>
+                    {children} {/* Render the children here */}
+                </Box>
+            </Box>
+        </ThemeProvider>
     );
 };
 
